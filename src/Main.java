4d25ff044ff1fef74a01f9f66004efdb2cadaafc
PS <ps-667@mail.ru>
2022-02-03 21:54:48 +0300

    Bonus Miles

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 3498;
+        int bonusMilePrice = 20;
+
+
+        int bonusMiles = ticketPrice / bonusMilePrice;
+        System.out.println(bonusMiles + " - количество бонусных миль");
+
+    }
+}
